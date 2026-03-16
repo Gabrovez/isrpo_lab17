a6eb6a1128f26543b25693633fdb1c2d7a1f4e79
Gabrovez <gabrovez321@gmail.com>
2026-03-16 20:20:38 +0300

    Step 8: functions and modular programming

---
+++ code/Step8_Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿//Функция сложения
+static double Add(double a, double b){
+    return a + b;
+}
+//Функция вычитания
+static double Subtract(double a, double b){
+    return a - b;
+}
+//Функция умножения
+static double Multiply(double a, double b){
+    return a * b;
+}
+//Функция деления
+static double Divide(double a, double b){
+    return a / b;
+}
+
+//Ввод данных
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+//Вывод ответа
+Console.WriteLine($"Сложение: {Add(num1, num2)}");
+Console.WriteLine($"Вычитание: {Subtract(num1, num2)}");
+Console.WriteLine($"Умножение: {Multiply(num1, num2)}");
+Console.WriteLine($"Деление: {Divide(num1, num2)}");
