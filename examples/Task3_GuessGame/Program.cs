1442fd864d24fbd86b48224637280e136abbc351
Gabrovez <gabrovez321@gmail.com>
2026-03-17 20:07:57 +0300

    Task 3 complete

---
+++ examples/Task3_GuessGame/Program.cs
@@ -0,0 +1,22 @@
+﻿// Создание переменных
+System.Random random = new System.Random();
+int x = random.Next(0, 101);
+int att = 0;
+// Ввод n
+System.Console.Write("Введите число: ");
+int n = int.Parse(System.Console.ReadLine());
+// Угадывание числа
+while (x != n){
+    att = att + 1;
+    System.Console.Write("Введите число: ");
+    if (n == x){ }
+    else if (n < x){
+        System.Console.WriteLine("Больше");
+    }
+     else{
+        System.Console.WriteLine("Меньше");
+    }
+    n = int.Parse(System.Console.ReadLine());
+}
+// Вывод ответа
+System.Console.WriteLine("Вы угадали!");
