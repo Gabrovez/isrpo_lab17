7687a8cfb6677031e1b79d34e60542f12e6fd13f
Gabrovez <gabrovez321@gmail.com>
2026-03-17 13:29:31 +0300

    Task 2 completed

---
+++ examples/Task2_isPrime/Program.cs
@@ -0,0 +1,24 @@
+﻿// Ввод данных
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+// Проверка простого числа
+static bool IsPrime(int n){
+    if (n <= 1){
+    return false;
+    }
+    for (int i = 2; i * i <= n; i++){
+        if (n % i == 0){
+            return false;
+        }
+    }    
+    return true;
+}
+// вывод данных
+bool result = IsPrime(number);
+if (result){
+    Console.WriteLine($"{number} - простое число");
+}
+else{
+    Console.WriteLine($"{number} - не простое число");
+}
+    
